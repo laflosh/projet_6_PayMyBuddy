e6c6d4bd5f5eaf6522e4e11daf8ad3f76b506b70
laflosh <l_florent_l@hotmail.fr>
2024-10-28 11:46:49 +0100

    Logging and javadoc for crud method user

--- backend/src/main/java/com/paymybuddy/backend/controller/UserController.java
+++ backend/src/main/java/com/paymybuddy/backend/controller/UserController.java
@@ -2,6 +2,8 @@ package com.paymybuddy.backend.controller;
 
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,47 +21,88 @@ import com.paymybuddy.backend.service.UserService;
 @RequestMapping("/api")
 public class UserController {
 
+	private Logger log = LogManager.getLogger(UserController.class);
+	
 	@Autowired
 	UserService userService;
 	
+	/**
+	 * Fetching all the users in the database
+	 * 
+	 * @return A List of users
+	 */
 	@GetMapping("/users")
 	public Iterable<User> getAllUsers(){
 		
+		log.info("Trying to acces to all users in database.");
 		return userService.getAllUsers();
 		
 	}
 	
+	/**
+	 * Fetching one user by his id
+	 * 
+	 * @param id of the user
+	 * @return A User
+	 */
 	@GetMapping("/users/{id}")
 	public Optional<User> getOneUser(@PathVariable int id) {
 		
+		log.info("Trying to acces to the user in the database with id : " + id + " .");
 		return userService.getOneUser(id);
 		
 	}
 	
+	/**
+	 * Saving a new user in db with entity sent in the body of the request
+	 * 
+	 * @param A new user
+	 * @return A saved user
+	 */
 	@PostMapping("/users")
 	public User addNewUser(@RequestBody User user) {
 		
+		log.info("Trying to saving a new user in the database.");
 		return userService.addNewUser(user);
 		
 	}
 	
+	/**
+	 * Updating an user in the db depending of the id in the entity
+	 * 
+	 * @param A modify user
+	 * @return The updated user
+	 */
 	@PutMapping("/users")
 	public User updateAExistingUser(@RequestBody User user) {
 		
+		log.info("Trying to updating an existing user in database with id : "+ user.getId() + " .");
 		return userService.updateAExistingUser(user);
 		
 	}
 	
+	/**
+	 * Delete an user in the db with the entity
+	 * 
+	 * @param A user
+	 */
 	@DeleteMapping("/users")
 	public void deleteAExistingUser(@RequestBody User user) {
 		
+		log.info("Trying to delete an user with entity in database.");
 		userService.deleteAExistingUser(user);
 		
 	}
 	
+	/**
+	 * Delete An user in the db depending of the id
+	 * 
+	 * @param id of the user
+	 */
 	@DeleteMapping("/users/{id}")
 	public void deleteAExistingUserById(@PathVariable int id) {
 		
+		log.info("Trying to delete an user in the database with id :" + id + ".");
 		userService.deleteAExistingUserById(id);
 		
 	}
--- backend/src/main/java/com/paymybuddy/backend/service/UserService.java
+++ backend/src/main/java/com/paymybuddy/backend/service/UserService.java
@@ -2,6 +2,8 @@ package com.paymybuddy.backend.service;
 
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,41 +13,70 @@ import com.paymybuddy.backend.repository.UserRepository;
 @Service
 public class UserService {
 
+	private Logger log = LogManager.getLogger(UserService.class);
+	
 	@Autowired
 	UserRepository userRepository;
 	
+	/**
+	 * @return A List Of users 
+	 */
 	public Iterable<User> getAllUsers(){
 		
+		log.info("Fetching all the users in the database");
 		return userRepository.findAll();
 		
 	}
 
+	/**
+	 * @param id of the user
+	 * @return An user
+	 */
 	public Optional<User> getOneUser(int id) {
 
+		log.info("Fetching one user in the database with id :" + id);
 		return userRepository.findById(id);
 		
 	}
 
+	/**
+	 * @param A new user
+	 * @return The saved user
+	 */
 	public User addNewUser(User user) {
 
+		log.info("Saving a new user in the database");
 		return userRepository.save(user);
 		
 	}
 
+	/**
+	 * @param A modify user
+	 * @return A updated user
+	 */
 	public User updateAExistingUser(User user) {
 		
+		log.info("Updating the user in the database with id :" + user.getId());
 		return userRepository.save(user);
 		
 	}
 	
+	/**
+	 * @param An user
+	 */
 	public void deleteAExistingUser(User user) {
 		
+		log.info("Delete an user in the database with his entity");
 		userRepository.delete(user);
 		
 	}
 
+	/**
+	 * @param id of a user
+	 */
 	public void deleteAExistingUserById(int id) {
 		
+		log.info("Delete an user in the database with id :" + id);
 		userRepository.deleteById(id);
 		
 	}
