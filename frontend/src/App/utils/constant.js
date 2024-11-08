e09ac649d1c7c5dc82ace81006a346365bf69f30
laflosh <l_florent_l@hotmail.fr>
2024-11-08 17:14:06 +0100

    Constant for App routes

---
+++ frontend/src/App/utils/constant.js
@@ -0,0 +1,13 @@
+const API_URL = "http//localhost:8081";
+
+export const API_ROUTES = {
+
+};
+
+export const APP_ROUTES = {
+    SIGN_IN : "/",
+    SIGN_UP : "/inscription",
+    CONNECTION : "/connection",
+    TRANSFER: "/transfer",
+    PROFIL: "/profil"
+};
