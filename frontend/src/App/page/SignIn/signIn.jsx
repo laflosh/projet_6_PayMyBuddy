9d32c1261ca211de94d6ae93a9160861ea882164
laflosh <l_florent_l@hotmail.fr>
2024-11-08 18:47:56 +0100

    function redirection sing in to inscription page

--- frontend/src/App/page/SignIn/signIn.jsx
+++ frontend/src/App/page/SignIn/signIn.jsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { APP_ROUTES } from "../../utils/constant";
 
 function SignIn(){
+
+    const navigate = useNavigate();
+
+    function redirectionTo(path){
+
+        navigate(`${path}`);
+
+    }
 
     return(
 
@@ -23,7 +33,9 @@ function SignIn(){
 
             </form>
 
-            <button class = "countainerSignIn__btnToInscription">
+            <button class = "countainerSignIn__btnToInscription"
+                onClick={redirectionTo(APP_ROUTES.SIGN_UP)}
+                >
                 M'inscrire
             </button>
 
