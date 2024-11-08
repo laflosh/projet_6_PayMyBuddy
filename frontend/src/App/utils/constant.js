590adce3afcce36000e2a98c4bc1ac3574c53fdd
laflosh <l_florent_l@hotmail.fr>
2024-11-08 19:56:20 +0100

    Add constants for API routes

--- frontend/src/App/utils/constant.js
+++ frontend/src/App/utils/constant.js
@@ -1,7 +1,13 @@
 const API_URL = "http//localhost:8081";
 
 export const API_ROUTES = {
-
+    LOG_IN:`${API_URL}/login`,
+    LOG_OUT:`${API_URL}/logout`,
+    USERS:`${API_URL}/api/users`,
+    USER_CONNECTIONS:`${API_URL}/api/users/:id/connections`,
+    TRANSACTIONS:`${API_URL}/api/transactions`,
+    SENDER_TRANSACTIONS:`${API_URL}/api/transactions/sender/:id`,
+    RECEIVER_TRANSACTIONS:`${API_URL}/api/transactions/receiver/:id`,
 };
 
 export const APP_ROUTES = {
@@ -9,5 +15,5 @@ export const APP_ROUTES = {
     SIGN_UP : "/inscription",
     CONNECTION : "/connection",
     TRANSFER: "/transfer",
-    PROFIL: "/profil"
+    PROFIL: "/profil",
 };
