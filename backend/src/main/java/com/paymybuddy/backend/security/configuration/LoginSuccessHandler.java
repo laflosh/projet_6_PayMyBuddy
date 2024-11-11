3400c6533dc6b81791bde7423b1e236f38fcd674
laflosh <l_florent_l@hotmail.fr>
2024-11-11 16:14:04 +0100

    Class for sending data about connected user for front

---
+++ backend/src/main/java/com/paymybuddy/backend/security/configuration/LoginSuccessHandler.java
@@ -0,0 +1,42 @@
+package com.paymybuddy.backend.security.configuration;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paymybuddy.backend.security.CustomUserDetails;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException{
+		
+		CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+		
+		Map<String, Object> userInfo = new HashMap<String, Object>();
+		userInfo.put("id", userDetails.getId());
+		userInfo.put("email", userDetails.getUsername());
+		userInfo.put("roles", userDetails.getAuthorities());
+		userInfo.put("message", "Authentication successful");
+		
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        
+        response.getWriter().write(objectMapper.writeValueAsString(userInfo));
+        
+	}
+	
+}
