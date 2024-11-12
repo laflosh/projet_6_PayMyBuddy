2cbc8cb9a537e0dca576fc78f0df3986d6ab5f07
laflosh <l_florent_l@hotmail.fr>
2024-11-12 18:57:43 +0100

    Functions for managing data in local storage

--- frontend/src/App/lib/common.js
+++ frontend/src/App/lib/common.js
@@ -5,3 +5,27 @@ export function redirectionTo(navigate, path){
     navigate(path);
 
 }
+
+export function setItemInLocalStorage(key, value){
+
+    localStorage.setItem(key, value);
+
+}
+
+export function getItemInLocalStorage(key){
+
+    return localStorage.getItem(key);
+
+}
+
+export function deleteItemInLocalStorage(key){
+
+    localStorage.removeItem(key);
+
+}
+
+export function clearLocalStorage(){
+
+    localStorage.clear();
+
+}
