c239992dfe7404098adb1f928e35760e4b4526ce
laflosh <l_florent_l@hotmail.fr>
2024-11-21 19:42:10 +0100

    Structure and page layout for error page

--- frontend/src/App/page/Error/error.jsx
+++ frontend/src/App/page/Error/error.jsx
@@ -1,7 +1,20 @@
 
 function Error(){
 
+    return(
 
+        <div className="countainer_error">
+
+            <p className="countainer_error__404">404</p>
+
+            <p className="countainer_error__text">
+                La page que vous cherchez n'existe pas.<br/>
+                Pour retourner sur le site Pay My Buddy, utilisez la bar de navigation.
+            </p>
+
+        </div>
+
+    )
 
 }
 
